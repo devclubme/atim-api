373c9543c7613d9e8209e207ada9b0dd4c468ee3
Dragan Cabarkapa <caboocg@gmail.com>
2019-07-05 16:21:14 +0200

    Initial commit

---
+++ libs/response-lib.js
@@ -0,0 +1,19 @@
+export function success(body) {
+  return buildResponse(200, body);
+}
+
+export function failure(body) {
+  return buildResponse(500, body);
+}
+
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Cache-Control": "no-cache"
+    },
+    body: JSON.stringify(body)
+  };
+}
