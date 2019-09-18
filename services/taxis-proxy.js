abeeccb643327108764b52b702efe4264b126064
Djordje Lacmanovic <djordje.lacmanovic@gmail.com>
2019-09-18 20:02:26 +0200

    add query of taxis via graphql

--- services/taxis-proxy.js
+++ services/taxis-proxy.js
@@ -3,15 +3,6 @@ import {
 } from 'graphql-request'
 
 export class TaxisProxy {
-  constructor() {
-    this._taxisProxyQuery = `{
-            taxis {
-              company(companyId: $companyId) {
-                companyName
-               }
-             }
-           }`;
-  }
 
   getCompany(companyId) {
     let result = await request(process.env.taxisServiceEndpoint, this._taxisProxyQuery, {
@@ -19,5 +10,53 @@ export class TaxisProxy {
     });
     // maybe do something with the result (like map into our own model?)
     return result;
+  }
+  
+  constructor() {
+    this._taxisProxyQuery = `
+    query CompanyById($companyId : String!){
+      taxis {
+        company(companyId: $companyId) {
+          companyName
+          businessSectorCode
+          businessSectorName
+          companyAddress
+          companyId
+          companyRegisteredForVat
+          companyRegistrationDate
+          companyRegistrationInstitution
+          companyRegistrationId
+          companyVatRegistrationDate
+          companyVatRegistrationId
+          taxArea
+          financialStatements {
+            details {
+              statementYear
+              statementToDate
+              statementId
+              statementFromDate
+              statementDate
+              statementCashFlowMethod
+              statementAuthorUnid
+              statementAuthorName
+              statementAuthorEmail
+              consolidatedStatement
+              companyOfficialUnid
+              companyOfficialLastName
+              companyOfficialFirstName
+              companyName
+              companyLocation
+              companyId
+              changeType
+              businessSectorCode
+            }
+            statementId
+            year
+          }
+        }
+        institution
+        version
+      }
+    }`;
   }
 }
