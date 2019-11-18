c2efb351fbe2c69f2c5247f3802a5eb6613b0c6c
Dragan Cabarkapa <caboocg@gmail.com>
2019-11-18 19:55:30 +0100

    Update sls version and run npm install on deploy due to serverless-webpack

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-npm install -g serverless@1.46.1
+npm install -g serverless@1.49.0
+npm install --silent --no-progress
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
