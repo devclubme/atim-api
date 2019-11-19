2829f1a2c75046bf755bba05023a39179e130680
Dragan Cabarkapa <caboocg@gmail.com>
2019-11-19 11:43:21 +0100

    Do package and deploy at deploy stage

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 npm install -g serverless@1.49.0
 echo $CODEBUILD_SRC_DIR
 echo $env
-serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
+serverless deploy --stage $env -v -r us-east-1
